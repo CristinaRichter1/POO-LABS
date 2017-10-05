d02be560cf4dbec9b59db14a1fbffa9104e14576
Cosmin <cartas.cosmin@gmail.com>
2017-10-05 13:01:44 +0300

    Seminar01 added

---
+++ Seminar01/Seminar01/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void increment(short int a) {
+	printf("Incrementarea variabilei se face la adresa %p \n", &a);
+	a++;
+}
+
+void increment1(short int &a) {
+	printf("Incrementarea variabilei se face la adresa %p \n", &a);
+	a++;
+}
+
+void increment2(short int* a) {
+	printf("Incrementarea variabilei se face la adresa %p \n", &a);
+	(*a)++;
+}
+
+void main() {
+	short int x = 9;
+
+	printf("Before increment x=%d \n", x);
+	printf(" Adresa lui x este %p \n", &x);
+	increment(x);
+	printf("After increment x=%d \n", x);
+
+	printf("Before increment x=%d \n", x);
+	increment1(x);
+	printf("After increment x=%d \n", x);
+
+	printf("Before increment x=%d \n", x);
+	increment2(&x);
+	printf("After increment x=%d \n", x);
+}
